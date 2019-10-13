33ee918f42d790b102982942195a6d130502c73e
Tamás Cserteg <csertegt@gmail.com>
2019-10-13 17:20:13 +0200

    bump

--- CSGs/packagewriting.jl
+++ CSGs/packagewriting.jl
@@ -15,13 +15,13 @@ subnode1 = CSGNode(sf1, ())
 subnode2 = CSGNode(sf2, ())
 
 
-n1 = CSGNode(CSGBuilding.union, (subnode1, subnode2))
+nd1 = CSGNode(CSGB.union, (subnode1, subnode2))
 
-n2 = CSGNode(CSGBuilding.union, (n1, CSGNode(CSGBuilding.complement, (subnode1, ))))
+nd2 = CSGNode(CSGB.union, (nd1, CSGNode(CSGB.complement, (subnode1, ))))
 
-n3 = CSGNode(CSGBuilding.subtraction, (n1, CSGNode(CSGBuilding.complement, (subnode1, ))))
+nd3 = CSGNode(CSGB.subtraction, (nd1, CSGNode(CSGB.complement, (subnode1, ))))
 
-n4 = CSGNode(CSGBuilding.subtraction, (subnode1, subnode2))
+nd4 = CSGNode(CSGB.subtraction, (subnode1, subnode2))
 
 ## Demo
 using StaticArrays, AbstractTrees, CSGBuilding
@@ -36,9 +36,9 @@ subnode1 = CSGNode(sf1, ())
 subnode2 = CSGNode(sf2, ())
 
 # trees
-n1 = CSGNode(CSGBuilding.union, (subnode1, subnode2))
-n2 = CSGNode(CSGBuilding.union, (n1, CSGNode(CSGBuilding.complement, (subnode1, ))))
-n3 = CSGNode(CSGBuilding.subtraction, (n1, CSGNode(CSGBuilding.complement, (subnode1, ))))
+n1 = CSGNode(CSGB.union, (subnode1, subnode2))
+n2 = CSGNode(CSGB.union, (n1, CSGNode(CSGB.complement, (subnode1, ))))
+n3 = CSGNode(CSGB.subtraction, (n1, CSGNode(CSGB.complement, (subnode1, ))))
 
 print_tree(n1)
 evaluate(n1, v1)
@@ -53,8 +53,22 @@ writeparaviewformat(sf0, "sphere5", (-10, 10, 100))
 
 sf01 = ImplicitSphere(SVector(2.5,2.5,2.5), 5.)
 nd01 = CSGNode(sf01, ())
-compnd01 = CSGNode(CSGBuilding.complement, (nd01, ))
-tree01 = CSGNode(CSGBuilding.union, (nd0, compnd01))
-tree01 = CSGNode(CSGBuilding.subtraction, (nd0, nd01))
+compnd01 = CSGNode(CSGB.complement, (nd01, ))
+tree01 = CSGNode(CSGB.union, (nd0, compnd01))
+tree01 = CSGNode(CSGB.subtraction, (nd0, nd01))
 
 writeparaviewformat(tree01, "sphere5", (-10, 10, 100))
+
+
+## Cylinder
+
+cyn =  ImplicitCylinder(SVector(0,0,1), SVector(0,0,0),1)
+writeparaviewformat(CSGNode(cyn, ()), "cyn", (-10, 10, 100))
+
+evaluate(cyn, SVector(-1,0,10))
+
+p = [0,0,0];
+d = [0,0,1];
+q = [1,0,1];
+
+CSGB.vectorfromline(p, d, [1,0,0])
