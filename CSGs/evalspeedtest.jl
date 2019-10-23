22600825f9f4d55879831696061e56a600339b8c
Tamás Cserteg <csertegt@gmail.com>
2019-10-23 23:19:27 +0200

    beeeenchmark

--- CSGs/evalspeedtest.jl
+++ CSGs/evalspeedtest.jl
@@ -1,6 +1,7 @@
 using StaticArrays
 using AbstractTrees
 using Random
+using BenchmarkTools
 using Revise
 using CSGBuilding
 const CSGB = CSGBuilding
@@ -94,25 +95,101 @@ function b5(surfaces, rpoints, treenum=10, treedepth=10)
     return sp
 end
 
+function c2f(cwrap)
+    expr = Expr(:function,
+        Expr(:tuple,
+        cwrap.Params[1],
+        cwrap.Params[2]),
+        Expr(:block,cwrap.Core...))
+    return eval(expr)
+end
+
+function b6(surfaces, rpoints, treenum=10, treedepth=10)
+    cnodes, cvals, _ = cachenodes(surfaces, rpoints);
+    Random.seed!(1234);
+    rcs_ = [randomcachedtree(cnodes, treedepth) for i in 1:treenum]
+    sp = 0.
+    for i in eachindex(rcs_)
+        f = c2f(tree2code(rcs_[i]))
+        for j in eachindex(cvals)
+            ev = Base.invokelatest(f, cvals, j)::CachedResult
+            sp += value(ev)
+        end
+    end
+    return sp
+end
+
+function b7(surfaces, rpoints, treenum=10, treedepth=10)
+    cnodes, cvals, _ = cachenodes(surfaces, rpoints);
+    Random.seed!(1234);
+    rcs_ = [randomcachedtree(cnodes, treedepth) for i in 1:treenum]
+    sp = 0.
+    for i in eachindex(rcs_)
+        f = tree2func(rcs_[i])
+        for j in eachindex(cvals)
+            ev = Base.invokelatest(f, cvals, j)::CachedResult
+            sp += value(ev)
+        end
+    end
+    return sp
+end
+
+function b8(surfaces, rpoints, treenum=10, treedepth=10)
+    cnodes, cvals, _ = cachenodes(surfaces, rpoints);
+    Random.seed!(1234);
+    rcs_ = [randomcachedtree(cnodes, treedepth) for i in 1:treenum]
+    sp = 0.
+    es = [tree2func(r) for r in rcs_]
+    for i in eachindex(es)
+        f = es[i]
+        for j in eachindex(cvals)
+            sp += value(Base.invokelatest(f, cvals, j)::CachedResult)
+        end
+    end
+    return sp
+end
+
+function b9(surfaces, rpoints, treenum=10, treedepth=10)
+    cnodes, cvals, _ = cachenodes(surfaces, rpoints);
+    Random.seed!(1234);
+    rcs_ = [randomcachedtree(cnodes, treedepth) for i in 1:treenum]
+    sp = 0.
+    es = [tree2func(r) for r in rcs_]
+    for i in eachindex(es)
+        f = es[i]
+        for j in eachindex(cvals)
+            ev = Base.invokelatest(f, cvals, j)::CachedResult
+            sp += value(ev)
+        end
+    end
+    return sp
+end
+
 surfac = makeit();
 Random.seed!(1234)
-points = [rand(3) for i in 1:100];
+points = [rand(3) for i in 1:100000];
 
-using BenchmarkTools
-
-evaltupl = (surfac, points, 10, 10)
+evaltupl = (surfac, points, 30, 10)
 
 b1(evaltupl...)
 b2(evaltupl...)
 b3(evaltupl...)
 b4(evaltupl...)
 b5(evaltupl...)
+b6(evaltupl...)
+b7(evaltupl...)
+b8(evaltupl...)
+b9(evaltupl...)
 
 res1 = @benchmark b1($evaltupl...)
 res2 = @benchmark b2($evaltupl...)
 res3 = @benchmark b3($evaltupl...)
 res4 = @benchmark b4($evaltupl...)
 res5 = @benchmark b5($evaltupl...)
+res6 = @benchmark b6($evaltupl...)
+res7 = @benchmark b7($evaltupl...)
+res8 = @benchmark b8($evaltupl...)
+res9 = @benchmark b9($evaltupl...)
 
-bm = [res1, res2, res3, res4, res5];
-prettyprint(bm, :s)
+bm = [res1, res2, res3, res4, res5, res6, res7, res8, res9];
+prettyprint(bm, :ms, :KiB)
--- CSGs/prettyprinter.jl
+++ CSGs/prettyprinter.jl
@@ -2,20 +2,21 @@ using PrettyTables
 using BenchmarkTools
 using Logging
 const tdict = Dict(:μs=>1000, :ms=>1000000, :s=>1000000000);
+const mdict = Dict(:KiB=>1024, :MiB=>1024*1024, :GiB=>1024*1024*1024);
 
-function evalBs(B, timeres=:ms)
+function evalBs(B, timeres=:ms, memres=:KiB)
     result = Array{Float64}(undef,size(B,1),7)
 	rest = tdict[timeres]
-
+	memr = mdict[memres]
     for i in eachindex(B)
-        result[i,:] = [i, minimum(B[i]).time/rest, BenchmarkTools.median(B[i]).time/rest, BenchmarkTools.mean(B[i]).time/rest, BenchmarkTools.maximum(B[i]).time/rest, allocs(B[i]), memory(B[i])/1024]
+        result[i,:] = [i, minimum(B[i]).time/rest, BenchmarkTools.median(B[i]).time/rest, BenchmarkTools.mean(B[i]).time/rest, BenchmarkTools.maximum(B[i]).time/rest, allocs(B[i]), memory(B[i])/(memr)]
     end
     return result
 end
 
-function prettyprint(benchArr, timeres=:ms)
-	headTab = ["parse" "minimum time" "median time" "mean time" "maximum time" "allocs" "memory"; "" "TIME" "TIME" "TIME" "TIME" "" "KiB"]
+function prettyprint(benchArr, timeres=:ms, memres=:KiB)
+	headTab = ["parse" "minimum time" "median time" "mean time" "maximum time" "allocs" "memory"; "" "TIME" "TIME" "TIME" "TIME" "" String(memres)]
 	ht = replace(headTab, "TIME"=>String(timeres))
-	pretty_table(evalBs(benchArr, timeres), ht, formatter=ft_round(3))
+	pretty_table(evalBs(benchArr, timeres, memres), ht, formatter=ft_round(3))
 end
 @info "Use prettyprint(benchArr) to print the results."
