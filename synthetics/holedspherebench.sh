697401674094bf9b4cd16611194fe513d2f5192c
Tamás Cserteg <Tams Cserteg>
2019-10-27 15:20:07 +0000

    fix .sh

--- synthetics/holedspherebench.sh
+++ synthetics/holedspherebench.sh
@@ -1,4 +1,4 @@
 export JULIA_NUM_THREADS=2
-julia --color=yes holedspherebench.jl
+julia --project --color=yes holedspherebench.jl
 export JULIA_NUM_THREADS=1
-julia --color=yes holedspherebench.jl
+julia --project --color=yes holedspherebench.jl
